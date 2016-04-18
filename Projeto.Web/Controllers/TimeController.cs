ab1287fbb37ad8badb0d7b4c500ddc0b90b5af7a
taysontey <taysontey@gmail.com>
2016-04-18 12:16:12 -0300

    Cadastrar Time(1)

---
+++ Projeto.Web/Controllers/TimeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+using Projeto.Web.Models;
+using Projeto.Web.ServiceReferenceTime;
+
+namespace Projeto.Web.Controllers
+{
+    public class TimeController : Controller
+    {
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [WebMethod]
+        public JsonResult Cadastrar(TimeModelCadastro model)
+        {
+            try
+            {
+                using(TimeServiceClient service = new TimeServiceClient())
+                {
+                    service.Cadastrar(model);
+                }
+
+                return Json("Time cadastrado.");
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message);
+            }
+        }
+    }
+}

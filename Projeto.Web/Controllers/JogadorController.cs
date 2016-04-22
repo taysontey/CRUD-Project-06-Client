1cc14b9812dc7ce5168613443122be9fb574e20e
taysontey <taysontey@gmail.com>
2016-04-22 11:51:41 -0300

    Cadastrar Jogador(1)

---
+++ Projeto.Web/Controllers/JogadorController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto.Web.ServiceReferenceJogador;
+
+namespace Projeto.Web.Controllers
+{
+    public class JogadorController : Controller
+    {
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public JsonResult Cadastrar(JogadorModelCadastro model)
+        {
+            try
+            {
+                using (JogadorServiceClient service = new JogadorServiceClient())
+                {
+                    service.Cadastrar(model);
+                }
+
+                return Json("Jogador cadastrado.");
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message);
+            }
+        }
+
+        public JsonResult CarregarTimes()
+        {
+            try
+            {
+                using (JogadorServiceClient service = new JogadorServiceClient())
+                {
+                    return Json(service.CarregarTimes());
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message);
+            }
+        }
+    }
+}
